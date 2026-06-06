8a3569004c6e3dfb090b3f0c92d30696cda53c65
abdul latif <abdullatifkhaleqi@gmail.com>
2026-06-06 17:06:38 +0430

    uodate user and auth route

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -12,76 +12,12 @@ import { authMiddlware } from "../../middlewares/auth.middlwares.js";
 
 const router = express.Router();
 
-/**
- * @openapi
- * /auth/register:
- *   post:
- *     summary: Register user
- *     tags:
- *       - Auth
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - firstName
- *               - lastName
- *               - email
- *               - password
- *             properties:
- *               firstName:
- *                 type: string
- *               lastName:
- *                 type: string
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       201:
- *         description: User created successfully
- *       400:
- *         description: Validation error
- *       409:
- *         description: User already exists
- */
 router.post(
   "/register",
   validateRequest(registerSchema),
   AuthController.registerUser,
 );
 
-/**
- * @openapi
- * /auth/login:
- *   post:
- *     summary: Register user
- *     tags:
- *       - Auth
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *               - password
- *             properties:
- *               email:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       201:
- *         description: User created successfully
- *       400:
- *         description: Validation error
- *       409:
- *         description: User already exists
- */
 router.post("/login", validateRequest(loginSchema), AuthController.loginUser);
 
 router.post(
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,32 +5,6 @@ import { authorize } from "../../middlewares/authorize.middleware.js";
 
 const router = express.Router();
 
-/**
- * @openapi
- * /user/getProfile:
- *   post:
- *     summary: get user
- *     tags:
- *       - User
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *             properties:
- *               email:
- *                 type: string
- *     responses:
- *       201:
- *         description: User created successfully
- *       400:
- *         description: Validation error
- *       409:
- *         description: User already exists
- */
 router.get("/user/me", authMiddlware, UserController.getUser);
 router.get("/", authMiddlware, authorize("admin"), UserController.getAllUsers);
 
